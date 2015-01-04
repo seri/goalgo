243f7b9368f7b5e225d18cfcda82f209f5370eb4
seri <seritrinh@gmail.com>
2015-01-04 16:04:29 +0700

    more retab

--- examples/dijkstra_example.go
+++ examples/dijkstra_example.go
@@ -13,14 +13,14 @@ func main() {
         "data/tinyEWD.txt",
         "data/mediumEWD.txt",
     }
-	var source = 0
+    var source = 0
     for _, filename := range filenames {
         fmt.Println("Running Dijkstra's shortest paths against " + filename)
         graf := graphU.ParseGraph(filename)
         sssp := graph.Dijkstra(graf, source)
         for target := 0; target < graf.V(); target++ {
-			fmt.Printf("%d to %d (%.2f) %v\n",
-					   source, target, sssp.DistTo(target), sssp.PathTo(target))
+            fmt.Printf("%d to %d (%.2f) %v\n",
+                       source, target, sssp.DistTo(target), sssp.PathTo(target))
         }
     }
 }
--- graph/sssp.go
+++ graph/sssp.go
@@ -1,58 +1,58 @@
 package graph
 
 import (
-	"container/list"
-	"math"
-	pq "github.com/seri/goalgo/pq/mutable"
+    "container/list"
+    "math"
+    pq "github.com/seri/goalgo/pq/mutable"
 )
 
 
 // SSSP stands for single-source shortest paths. It stores the shortest paths
 // from a chosen vertice (called the source) to every other vertice of a graph.
 type SSSP struct {
-	cost []float64
-	prev []int
+    cost []float64
+    prev []int
 }
 
 func newSSSP(nV int) *SSSP {
-	cost := make([]float64, nV)
-	prev := make([]int, nV)
-	for u := range cost {
-		cost[u] = math.Inf(1)
-		prev[u] = -1
-	}
-	return &SSSP { cost, prev }
+    cost := make([]float64, nV)
+    prev := make([]int, nV)
+    for u := range cost {
+        cost[u] = math.Inf(1)
+        prev[u] = -1
+    }
+    return &SSSP { cost, prev }
 }
 
 // Returns the shortest distance from source to the given target.
 func (me SSSP) DistTo(target int) float64 {
-	return me.cost[target]
+    return me.cost[target]
 }
 
 func listToIntSlice(l *list.List) []int {
-	slice := make([]int, 0)
-	for e := l.Front(); e != nil; e = e.Next() {
-		slice = append(slice, e.Value.(int))
-	}
-	return slice
+    slice := make([]int, 0)
+    for e := l.Front(); e != nil; e = e.Next() {
+        slice = append(slice, e.Value.(int))
+    }
+    return slice
 }
 
 // Lists all vertices in the shortest path from source to the given target.
 func (me SSSP) PathTo(target int) []int {
-	l := list.New()
-	for u := target; u != -1; u = me.prev[u] {
-		l.PushFront(u)
-	}
-	return listToIntSlice(l)
+    l := list.New()
+    for u := target; u != -1; u = me.prev[u] {
+        l.PushFront(u)
+    }
+    return listToIntSlice(l)
 }
 
 func (me *SSSP) relax(edge Edge) {
-	u, v := edge.From(), edge.To()
-	newCost := edge.Weight() + me.cost[u]
-	if newCost < me.cost[v] {
-		me.cost[v] = newCost
-		me.prev[v] = u
-	}
+    u, v := edge.From(), edge.To()
+    newCost := edge.Weight() + me.cost[u]
+    if newCost < me.cost[v] {
+        me.cost[v] = newCost
+        me.prev[v] = u
+    }
 }
 
 
@@ -64,27 +64,27 @@ func (me *SSSP) relax(edge Edge) {
 // The algorithm works because we always visit the vertices in the order of how
 // close they are to the source.
 func Dijkstra(graf *G, source int) *SSSP {
-	sssp := newSSSP(graf.V())
-	sssp.cost[source] = 0
+    sssp := newSSSP(graf.V())
+    sssp.cost[source] = 0
 
-	clone := make([]float64, graf.V())
-	copy(clone, sssp.cost)
-	queue := pq.New(pq.Reverse(pq.Float64Slice(clone)))
+    clone := make([]float64, graf.V())
+    copy(clone, sssp.cost)
+    queue := pq.New(pq.Reverse(pq.Float64Slice(clone)))
 
-	for !queue.Empty() {
-		u := queue.Pop()
-		for _, edge := range graf.Edges(u) {
-			v := edge.To()
-			newCost := edge.Weight() + sssp.cost[u]
-			if newCost < sssp.cost[v] {
-				sssp.cost[v] = newCost
-				sssp.prev[v] = u
-				queue.Change(v, newCost)
-			}
-		}
-	}
+    for !queue.Empty() {
+        u := queue.Pop()
+        for _, edge := range graf.Edges(u) {
+            v := edge.To()
+            newCost := edge.Weight() + sssp.cost[u]
+            if newCost < sssp.cost[v] {
+                sssp.cost[v] = newCost
+                sssp.prev[v] = u
+                queue.Change(v, newCost)
+            }
+        }
+    }
 
-	return sssp
+    return sssp
 }
 
 
@@ -96,14 +96,14 @@ func Dijkstra(graf *G, source int) *SSSP {
 // by topological order that there are no edges from u to any vertices already
 // visited.
 func AcyclicSP(graf *G, source int) *SSSP {
-	sssp := newSSSP(graf.V())
-	sssp.cost[source] = 0
-	for _, u := range TopoSort(graf) {
-		for _, edge := range graf.Edges(u) {
-			sssp.relax(edge)
-		}
-	}
-	return sssp
+    sssp := newSSSP(graf.V())
+    sssp.cost[source] = 0
+    for _, u := range TopoSort(graf) {
+        for _, edge := range graf.Edges(u) {
+            sssp.relax(edge)
+        }
+    }
+    return sssp
 }
 
 
@@ -117,14 +117,14 @@ func AcyclicSP(graf *G, source int) *SSSP {
 // shortest paths that contain at most k edges. This in turn can be proved by
 // induction.
 func BellmanFord(graf *G, source int) *SSSP {
-	sssp := newSSSP(graf.V())
-	sssp.cost[source] = 0
-	for i := 0; i < graf.V(); i++ {
-		for u := 0; u < graf.V(); u++ {
-			for _, edge := range graf.Edges(u) {
-				sssp.relax(edge)
-			}
-		}
-	}
-	return sssp
+    sssp := newSSSP(graf.V())
+    sssp.cost[source] = 0
+    for i := 0; i < graf.V(); i++ {
+        for u := 0; u < graf.V(); u++ {
+            for _, edge := range graf.Edges(u) {
+                sssp.relax(edge)
+            }
+        }
+    }
+    return sssp
 }
